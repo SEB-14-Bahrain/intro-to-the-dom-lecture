ecaac9f340195aa14b90897cec67d325fb535313
nayaba <nabila.ayaba@gmail.com>
2026-06-22 16:04:14 +0300

    Add ability to remove items from list

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ const containerEl = document.querySelector('#container')
 const inputEl = document.querySelector('input')
 
 /*--------------- Event Listeners ----------------*/
-btnEl.addEventListener('click', function(){
-// console.log(inputEl.value)
+btnEl.addEventListener('click', function(event){
    let pEl = document.createElement('p')
    pEl.textContent = inputEl.value
    containerEl.append(pEl)
    inputEl.value = ''
+})
+
+containerEl.addEventListener('click', function(event){
+    event.target.style.textDecoration = 'line-through'
+})
+
+containerEl.addEventListener('dblclick', function(event){
+    event.target.remove()
 })
 
 /*------------------ Functions -------------------*/
